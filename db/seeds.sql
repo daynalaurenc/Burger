f9cffde5fe98cc27f499a4c21d8ae7841a6394fe
DLC <daynalaurenc@gmail.com>
2019-03-30 23:49:47 -0700

    db connected to mysql work bench

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Paleo Choice');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Western');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan-delicious', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Up', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Banger', true);
+INSERT INTO burgers (burger_name) VALUES ('Bacon Lover');
