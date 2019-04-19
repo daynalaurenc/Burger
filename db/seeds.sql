5a5573e14d23425c79b136bf57db78a7a09ee26c
DLC <daynalaurenc@gmail.com>
2019-04-18 21:17:28 -0700

    finale

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Paleo Choice', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Western', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan-delicious', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Up', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Banger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Western', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan-delicious', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Up', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Banger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Lover', false);
